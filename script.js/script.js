1ef970875e4436d3aab39201f936806f250f13a2
smithexauceyayama <exauceyayama@gmail.com>
2025-05-30 17:59:28 +0100

    menu
    modification menu

--- script.js/script.js
+++ script.js/script.js
@@ -1,6 +1,6 @@
 const bouton = document.getElementById('btn-menu');
-const menu = document.getElementById('header-menu-masquer');
-const FermerMenu = document.getElementById('btn-fermer');
+const menu = document.getElementById('navbar-cache');
+const FermerMenu = document.getElementById('navbar-clicable');
 
 bouton.addEventListener('click', function() {
     menu.style.display = 'flex';
